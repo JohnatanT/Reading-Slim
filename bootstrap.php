807f882ada272a0a2f8088b0f28c0fa0878012d2
Francisco Johnatan <johnatan12014@hotmail.com>
2018-04-20 12:48:47 -0300

    Request e Response

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,18 @@
 <?php 
 
 use \Slim\App as App;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
 
 require __DIR__.'/vendor/autoload.php';
 
 $app = new App;
-$app->get('/hello',function (){
-    return "Hello Word";
+
+$app->get('/hello/{name}',function (Request $request, Response $response){
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, {$name}");
+    return $response;
 });
 
 $app->run();
